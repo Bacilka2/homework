e979c891393b8e7f2624528c1d1d3e49f5aa192a
bacilka <axis@m.r>
2025-07-18 21:06:29 +0500

    commit

--- src/masks.py
+++ src/masks.py
@@ -13,7 +13,7 @@ def get_mask_card_number(card_num: str) -> str:
     return result.strip()
 
 
-def get_mask_account_card(account_num: str) -> str:
+def get_mask_account(account_num: str) -> str:
     """Маска номера счета"""
     mask = "**"
     result = mask + account_num[-4:0]
