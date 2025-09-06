35edfe1e00dd2762b2d4195587683d4b1b48409f
bacilka <axis@m.r>
2025-09-06 20:06:05 +0500

    commit

--- src/external_api.py
+++ src/external_api.py
@@ -4,6 +4,7 @@ import requests
 
 import os
 from dotenv import load_dotenv, dotenv_values
+
 load_dotenv()
 
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import json
 import unittest
+
 from unittest.mock import patch, mock_open
 
 
